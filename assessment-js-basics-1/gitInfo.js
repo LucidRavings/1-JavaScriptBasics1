1f40854dafb3339a2dad24953adf9a5091b4df4a
LucidRavings <cybernerd82@yahoo.com>
2023-02-17 12:50:00 -0700

    answered all questions in gitInfo

--- assessment-js-basics-1/gitInfo.js
+++ assessment-js-basics-1/gitInfo.js
@@ -34,7 +34,7 @@ let gitInitDefinition = "git init is the command that applies the root git file 
 */
 
 //CODE HERE
-let gitCloneDefinition = ""
+let gitCloneDefinition = "git clone creates a copy of the git tracking, creating a branch in the git system that helps to prevent the information from one git repository from getting lost or destroyed by creating a back-up in another location."
 //////////////////PROBLEM 5////////////////////
 /*
     Create a variable called 'gitStatusDefinition'.  
@@ -42,7 +42,7 @@ let gitCloneDefinition = ""
 */
 
 //CODE HERE
-
+let gitStatusDefinition = "git status is the command used to check on the version status of your git enabled folders.  Telling the developer what files have been changed or commited, and how they may or may not match up with the GitHub repository."
 //////////////////PROBLEM 6////////////////////
 /*
     Create a variable called 'gitAddDefinition'.  
@@ -53,7 +53,8 @@ let gitCloneDefinition = ""
 */
 
 //CODE HERE
-
+let gitAddDefinition = "git add is used to specify what files or folders are to be prepared for commiting to git version control.  either by finishing the statment with a period.  Or by specifying the name of the file or folder."
+let gitAddCode "git add ."
 //////////////////PROBLEM 7////////////////////
 /*
     Create a variable called 'gitCommitDefinition'.  
@@ -64,8 +65,8 @@ let gitCloneDefinition = ""
 */
 
 //CODE HERE
-
-
+let gitCommitDefinition = "git commit is used to take files that have already been prepared by 'git add' and add them into the final version history for git.  allowing for tracking, retrieval, and storage."
+let gitCommitCode = 'git commit -m "initial commit"'
 //////////////////PROBLEM 8////////////////////
 /*
     Create a variable called 'gitPushDefinition'.  
@@ -73,3 +74,4 @@ let gitCloneDefinition = ""
 */
 
 //CODE HERE
+let gitPushDefinition = "git push is the command that works with the site GitHub in order to update the developer's online repositories with all the git commit's that the developer has produced."
