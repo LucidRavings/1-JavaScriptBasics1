9e6daa743a33d714a3cced3065708ed61a1f4096
LucidRavings <cybernerd82@yahoo.com>
2023-02-17 11:43:11 -0700

    sdlc problems 1 and 2 completed

--- assessment-js-basics-1/sdlc.js
+++ assessment-js-basics-1/sdlc.js
@@ -5,13 +5,12 @@
     Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
 */
 
-// Implementation
-// Design
-// Maintain
-// Analysis of Requirements
-// Testing & Integration
 // Planning
-
+// Analysis of Requirements
+// Design
+// Implementation
+// Testing & Integration
+// Maintain
 
 //////////////////PROBLEM 2////////////////////
 /*
@@ -19,3 +18,8 @@
     The first one is done for you. 
 */
 const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+const analysisOfRequirements = "Once the end-goal of the software has been defined and determined by the planning phase, the plan is analized further to check for exactly what needs to be done to acomplish the end-goal by accurately breaking-down the requirements of the project into specific parts."
+const design = "At this stage, the requirements produced from analysis are used to inform the programer with the nessasary knowledge to decide what tools and resources that will best acomplish the end-goal of design while also fulfilling the requirements of the project. Those resources and tools are gathered and prepared at this time, into a more actionable plan"
+const implementation = "Once the plan, requirements, and design have been established, the next phase is to begin implementation where the programers start writing/assembeling the many programs required to achomplish the end-goal into a working prototype/product"
+const testingAndIntegration = "After the software has been produced, it's important to throughly test and scrutinize for any lingering errors or bugs.  Once an acceptable degree of QA has been achomplished, the product is put into use"
+const maintain = "Over time, errors will either be discovered, or some other change will encouter the software.  This can cause code to malfunction, unless it is periodically corrected and maintained, either indefinitely or until the software is discontinued or replaced."
